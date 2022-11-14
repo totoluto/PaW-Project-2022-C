dbf3d17218fb7c8d1904b1c38bd1f4b2fe75b492
totoluto <94611749+totoluto@users.noreply.github.com>
2022-11-14 08:52:16 +0100

    basic map and nav created
    I created a basic route system and designed a few base components

--- logistics-of-world-politics/src/App.js
+++ logistics-of-world-politics/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Map from "./Sites/Map";
+import Error from "./Sites/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+      <div className="App">
+          <BrowserRouter>
+              <nav className={"App-nav"}>
+                  <ul className={"App-ul-nav"}>
+                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Karte</li></Link>
+                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
+                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
+                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
+                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
+                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
+                  </ul>
+              </nav>
+              <div>
+                  <Routes>
+                      <Route path="/" element={<Map/>}/>
+                      <Route path="/" element={<Map/>}/>
+                      <Route path="/" element={<Map/>}/>
+                      <Route path="/" element={<Map/>}/>
+                      <Route path="/" element={<Map/>}/>
+                      <Route path="*" element={<Error/>}/>
+                  </Routes>
+              </div>
+          </BrowserRouter>
+      </div>
   );
 }
 
