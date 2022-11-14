47c9389481b12770ac69d7affcdefddb66b2c917
totoluto <94611749+totoluto@users.noreply.github.com>
2022-11-14 09:24:43 +0100

    set visibility to hidden of unwelcomed tag

--- logistics-of-world-politics/src/Sites/Map.js
+++ logistics-of-world-politics/src/Sites/Map.js
@@ -6,7 +6,6 @@ function Map() {
         <div>
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
                 <TileLayer
-                    attribution='<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=DRREC6tXiaabcmu2WnkGIr9bA96PxlzHWF7j9pivBWhDbCjPsZp4jhmT8HJDGisi"
                 />
                 <Marker position={[51.505, -0.09]}>
