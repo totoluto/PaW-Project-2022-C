bab7221309bdabff4b4a4c044715d5e68efe9cac
totoluto <94611749+totoluto@users.noreply.github.com>
2022-12-18 17:48:25 +0100

    changed nav style and added text + added SVG overlays

--- logistics-of-world-politics/src/App.js
+++ logistics-of-world-politics/src/App.js
@@ -10,20 +10,25 @@ function App() {
               <nav className={"App-nav"}>
                   <ul className={"App-ul-nav"}>
                       <Link to="/" className={"Navlink"}><li className="App-li-nav">Karte</li></Link>
-                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
-                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
-                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
-                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
-                      <Link to="/" className={"Navlink"}><li className="App-li-nav">Other Stuff</li></Link>
+                      <Link to="/panamakanal" className={"Navlink"}><li className="App-li-nav">Panamakanal</li></Link>
+                      <Link to="/magellanstrasse" className={"Navlink"}><li className="App-li-nav">Magellanstrasse</li></Link>
+                      <Link to="/gibraltar" className={"Navlink"}><li className="App-li-nav">Gibraltar</li></Link>
+                      <Link to="/bosporus" className={"Navlink"}><li className="App-li-nav">Bosporus</li></Link>
+                      <Link to="/suezkanal" className={"Navlink"}><li className="App-li-nav">Suezkanal</li></Link>
+                      <Link to="/hormus" className={"Navlink"}><li className="App-li-nav">Strasse von Hormus</li></Link>
+                      <Link to="/malakka" className={"Navlink"}><li className="App-li-nav">Strasse von Malakka</li></Link>
                   </ul>
               </nav>
               <div>
                   <Routes>
                       <Route path="/" element={<Map/>}/>
-                      <Route path="/" element={<Map/>}/>
-                      <Route path="/" element={<Map/>}/>
-                      <Route path="/" element={<Map/>}/>
-                      <Route path="/" element={<Map/>}/>
+                      <Route path="/panamakanal" element={<Map/>}/>
+                      <Route path="/magellanstrasse" element={<Map/>}/>
+                      <Route path="/gibraltar" element={<Map/>}/>
+                      <Route path="/bosporus" element={<Map/>}/>
+                      <Route path="/suezkanal" element={<Map/>}/>
+                      <Route path="/hormus" element={<Map/>}/>
+                      <Route path="/malakka" element={<Map/>}/>
                       <Route path="*" element={<Error/>}/>
                   </Routes>
               </div>
---
+++ logistics-of-world-politics/src/Sites/Gibraltar.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Gibraltar() {
+    return(
+        <h1 className={"Error"}>
+            This Page does not exist
+        </h1>
+    );
+}
+
+export default Gibraltar;
