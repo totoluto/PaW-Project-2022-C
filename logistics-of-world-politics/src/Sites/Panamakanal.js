722d4f889b1cbb260c6a021afc2231ffd5b4e78a
DanielWilliamFaessler <daniel.faesslerasmodi@gmail.com>
2023-01-08 22:59:06 +0100

    Made some minor aesthetic changes. Also made the sources into hyperlinks

--- logistics-of-world-politics/src/Sites/Panamakanal.js
+++ logistics-of-world-politics/src/Sites/Panamakanal.js
@@ -14,7 +14,9 @@ function Panamakanal() {
     return(
         <div className="info">
             <h1>Panamakanal:</h1>
-            Der Panamakanal ist eine künstliche, rund 82 Kilometer lange Wasserstraße mit, die die Landenge von Panama in Mittelamerika durchschneidet, den Atlantik mit dem Pazifik für die Schifffahrt verbindet und ihr damit die Fahrt um das Kap Hoorn oder durch die Magellanstraße an der Südspitze Südamerikas erspart. Arbeiten begannen 1881 bzw. 1894. Der 1914 schließlich eröffnete Kanal mit Schleusen ist eine der wichtigsten Wasserstraßen der Welt; etwa 13.700 Schiffe (Mittel der Jahre 2017 bis 2019; 2011 bis 2013: 14.300) durchfahren ihn pro Jahr.
+            Der Panamakanal ist eine künstliche, rund 82 Kilometer lange Wasserstraße mit, die die Landenge von Panama in Mittelamerika durchschneidet, den Atlantik mit dem Pazifik für die Schifffahrt verbindet und ihr damit die Fahrt um das Kap Hoorn oder durch die Magellanstraße an der Südspitze Südamerikas erspart.
+            <br/>
+            Arbeiten begannen 1881 bzw. 1894. Der 1914 schließlich eröffnete Kanal mit Schleusen ist eine der wichtigsten Wasserstraßen der Welt; etwa 13.700 Schiffe (Mittel der Jahre 2017 bis 2019; 2011 bis 2013: 14.300) durchfahren ihn pro Jahr.
         </div>
     );
 }
